c498b19a8483510efd4a205789e2a977e290a254
Arjun Balaji <arjunblj@gmail.com>
2015-04-26 12:52:51 -0400

    Removed separate Minimax file.

--- play.py
+++ play.py
@@ -4,10 +4,10 @@ from players import *
 
 if __name__ == "__main__":
 
-    p1 = MinimaxAI(1, 2, 4)
+    p1 = Human(1, 2)
     # p2 = RandomAI(2, 1)
 
-    p2 = Human(1, 2)
+    p2 = MinimaxAI(1, 2, 4)
 
     game = Connect4Game(p1, p2)
     game.play()
